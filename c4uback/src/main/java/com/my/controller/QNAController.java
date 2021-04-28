72368fb1cd20fdee306c08dfb3ff22f486ec9a87
jaekyungkim <dev.jaekyungkim@gmail.com>
2021-04-28 17:50:08 +0900

    commit

--- c4uback/src/main/java/com/my/controller/QNAController.java
+++ c4uback/src/main/java/com/my/controller/QNAController.java
@@ -8,9 +8,11 @@ import java.util.Optional;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -158,4 +160,22 @@ public class QNAController {
 		return map;
 	}
 	
+	@PutMapping("/admin/qna/reply/{qna_id}")
+	public Map<String, Object> adminReply(@PathVariable int qna_id,
+										  @RequestBody QNA qna) throws Exception{
+		Map<String, Object> map = new HashMap<>();
+		service.modify(qna);
+		log.debug(qna);
+		map.put("status", 1);
+		return map;
+	}
+	
+	@DeleteMapping("/admin/qna/delete/{qna_id}")
+	public Map<String, Object> adminDelete(@PathVariable int qna_id) throws Exception{
+		Map<String, Object> map = new HashMap<>();
+		service.remove(qna_id);
+		log.debug(qna_id);
+		map.put("status", 1);
+		return map;
+	}
 }
